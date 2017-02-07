1a2e12339e1a318191d0421563f444a1b9e696f4
ramesh7128 <ramesh7128@gmail.com>
2017-02-07 07:43:50 +0530

    getting to the movie page based on user input

--- movies_scraper.py
+++ movies_scraper.py
@@ -5,21 +5,57 @@ import selenium.webdriver.support.ui as ui
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+import os
+
 
 driver = webdriver.Firefox(executable_path='/home/ramesh/Downloads/geckodriver')
-driver.get('https://www.spicinemas.in/')
-
-# elem.send_keys(Keys.RETURN)
-# assert "No results found" not in driver.page_source
-elem = driver.find_element_by_id("selectCity").click()
+driver.implicitly_wait(30)
+driver.maximize_window()
+driver.get('https://www.spicinemas.in/chennai/now-showing')
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'selectCity')))
+driver.find_element_by_id("selectCity").click()
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="login-topbar"]/a')))
 driver.find_element_by_xpath('//*[@id="login-topbar"]/a').click()
-time.sleep(5)
+
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="userEmail"]')))
 driver.find_element_by_xpath('//*[@id="userEmail"]').send_keys('ramesh7128@gmail.com')
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="userPassword"]')))
 driver.find_element_by_xpath('//*[@id="userPassword"]').send_keys('findout5')
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="login_submit"]')))
 driver.find_element_by_xpath('//*[@id="login_submit"]').click()
-time.sleep(5)
-driver.find_element_by_xpath('/html/body/div[1]/article/section[2]/div/div/section/section[1]/nav/section/ul/li[2]/a').click()
-time.sleep(10)
-movies_list = driver.find_elements_by_class_name("movie__item")
-for movie in movies_list:
-    movie.find_element_by_xpath("//a/img[title()='ATHEY KANGAL']").click()
+
+WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located((By.XPATH, "//div/dl/dt/a")))
+movies_list_now_showing = []
+movies_list = driver.find_elements_by_xpath('//div/dl/dt/a')
+for i in range(len(movies_list)):
+	movies_list = driver.find_elements_by_xpath('//div/dl/dt/a')
+	if not movies_list[i].text in movies_list_now_showing:
+		movies_list_now_showing.append(movies_list[i].text)
+	
+for i in range(len(movies_list_now_showing)):
+	print i, ": ", movies_list_now_showing[i]
+
+movie_code = raw_input('enter the movie code')
+movie_name = movies_list_now_showing[int(movie_code)]
+element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, movie_name)))
+element.click()
+
+
+# # time.sleep(5)
+
+# for movie in movies_list:
+# 	print movie.text
+# 	# try:
+# 	# 	element = movie.find_element_by_xpath('.//div/dl/dt/a')
+# 	# 	if element.text == 'XXX RETURN OF XANDER CAGE':
+# 	# 		element.click()
+# 	# 		break
+# 	# except StaleElementReferenceException as e:
+# 	# 	print e
+# 	# 	break
+
+
+# text_to_be_present	# 
+	
+
